bee75f765b2c3460e2af9be4caca6ba92d387a03
mihkuno <caindayjoeninyo@gmail.com>
2024-05-02 22:08:03 +0800

    added client-side card counting

--- service/visual_manipulation.py
+++ service/visual_manipulation.py
@@ -7,15 +7,8 @@ import time
 from ultralytics import YOLO
 
 
-model = YOLO('./runs/detect/train/weights/last.pt')
+model = YOLO('./runs/detect/train/weights/best.pt')
 
-
-card_values = {
-    'C2': 2, 'C3': 3, 'C4': 4, 'C5': 5, 'C6': 6, 'C7': 7, 'C8': 8, 'C9': 9, 'C10': 10, 'CA': 11, 'CJ': 10, 'CK': 10, 'CQ': 10,
-    'D2': 2, 'D3': 3, 'D4': 4, 'D5': 5, 'D6': 6, 'D7': 7, 'D8': 8, 'D9': 9, 'D10': 10, 'DA': 11, 'DJ': 10, 'DK': 10, 'DQ': 10,
-    'H2': 2, 'H3': 3, 'H4': 4, 'H5': 5, 'H6': 6, 'H7': 7, 'H8': 8, 'H9': 9, 'H10': 10, 'HA': 11, 'HJ': 10, 'HK': 10, 'HQ': 10,
-    'S2': 2, 'S3': 3, 'S4': 4, 'S5': 5, 'S6': 6, 'S7': 7, 'S8': 8, 'S9': 9, 'S10': 10, 'SA': 11, 'SJ': 10, 'SK': 10, 'SQ': 10
-}
 
 
 def from_b64(uri):
@@ -48,7 +41,7 @@ def detect(img):
     
     try:
         img = from_b64(img)
-        results = model.predict(img, conf=0.6)
+        results = model.predict(img, conf=0.75)
         
         for r in results:
             for box in r.boxes:
